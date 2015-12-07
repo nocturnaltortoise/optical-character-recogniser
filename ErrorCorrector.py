2abee31ca75496297860f0a5d50bc4c55f7c9501
Simon Turner <simonturner34@gmail.com>
2015-12-07 20:24:26 +0000

    initial commit

---
+++ ErrorCorrector.py
@@ -0,0 +1,73 @@
+import numpy as np
+
+
+class ErrorCorrector:
+
+    def __init__(self):
+        self.common_words = np.loadtxt('count_1w.txt', dtype={'names': ('word', 'frequency'),
+                                                              'formats': ('S20', np.int)})
+        self.frequencies = self.common_words["frequency"]
+        self.dictionary = self.common_words["word"]
+
+    def __calculate_edit_distance(self, word1, word2):
+
+        """Work out the number of operations to get from word1 to word2"""
+
+        edit_distance = 0
+
+        # if len(word1) != len(word2):
+        #     edit_distance += abs(len(word1) - len(word2))
+        if len(word1) < len(word2):
+            min_word_length = len(word1)
+        else:
+            min_word_length = len(word2)
+
+        for i in xrange(min_word_length):
+            if word1[i] != word2[i]:
+                # edit_distance += abs(ord(word1[i]) - ord(word2[i]))
+                edit_distance += 1
+
+        return edit_distance
+
+    def get_nearest_words(self, word1, dictionary):
+
+        """Given a word, find the nearest words (within 3 edits) by edit distance."""
+        # need to prioritise edit distance 1 words before 2 and 3, and only then consider frequency
+        # within a set of equal edit distance words
+        nearest_words = []
+        for word in dictionary:
+            edit_distance = self.__calculate_edit_distance(word1, str(word))
+            if edit_distance <= 2 and len(str(word)) == len(word1):  # edit distance threshold - most mistakes aren't more than three characters
+                # nearest_words.append((word, edit_distance))   # zipping edit_distances with the words so we can sort
+                nearest_words.append(str(word))
+        # nearest_words = [word for word in dictionary if calculate_edit_distance(word1,word) <= 3] #this kinda works
+        return nearest_words
+
+
+    def get_highest_freq_word(self, words, dictionary):
+
+        """Given a list of words, return the one with the highest frequency in the English language, or at least in my list of frequencies."""
+        # consider using collocates - i.e. nearby words as well, so frequencies can be considered in terms of word-pairs
+        for freq_word in dictionary:
+            for word in words:
+                if str(word) == str(freq_word):
+                    return freq_word
+                else:
+                    return word
+
+
+    # print get_highest_freq_word(get_nearest_words("tbe", dictionary), dictionary)
+
+    def correct_words(self, predicted_words, correct_words):
+        # incorrect_words = set(correct_words) - set(predicted_words)
+        corrected_words = []
+
+        for word in predicted_words:
+            if word not in correct_words:
+                near_words = self.get_nearest_words(word, self.dictionary)
+                if len(near_words) != 0:
+                    corrected_words.append(self.get_highest_freq_word(near_words, self.dictionary))
+            else:
+                corrected_words.append(word)
+
+        return corrected_words
---
+++ run_assignment.py
@@ -0,0 +1,178 @@
+import numpy as np
+import scipy.linalg as la
+from ErrorCorrector import *
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+import time
+
+# npy and dat files will actually be in the same directory as the code, not in data/
+page1 = np.load('data/train1.npy')
+page2 = np.load('data/train2.npy')
+page3 = np.load('data/train3.npy')
+page4 = np.load('data/train4.npy')
+
+test1 = np.load('data/test1.npy')
+test2 = np.load('data/test2.npy')
+
+page1_boxes = np.loadtxt('data/train1.dat', dtype={'names': ('labels', 'left', 'bottom', 'right', 'top', 'word_end'),
+                                                   'formats': ('S1', np.int, np.int, np.int, np.int, np.int)})
+page2_boxes = np.loadtxt('data/train2.dat', dtype={'names': ('labels', 'left', 'bottom', 'right', 'top', 'word_end'),
+                                                   'formats': ('S1', np.int, np.int, np.int, np.int, np.int)})
+page3_boxes = np.loadtxt('data/train3.dat', dtype={'names': ('labels', 'left', 'bottom', 'right', 'top', 'word_end'),
+                                                   'formats': ('S1', np.int, np.int, np.int, np.int, np.int)})
+page4_boxes = np.loadtxt('data/train4.dat', dtype={'names': ('labels', 'left', 'bottom', 'right', 'top', 'word_end'),
+                                                   'formats': ('S1', np.int, np.int, np.int, np.int, np.int)})
+test1_boxes = np.loadtxt('data/test1.dat', dtype={'names': ('labels', 'left', 'bottom', 'right', 'top', 'word_end'),
+                                                  'formats': ('S1', np.int, np.int, np.int, np.int, np.int)})
+test2_boxes = np.loadtxt('data/test2.dat', dtype={'names': ('labels', 'left', 'bottom', 'right', 'top', 'word_end'),
+                                                  'formats': ('S1', np.int, np.int, np.int, np.int, np.int)})
+
+plt.imshow(test1, cmap=cm.Greys_r)
+plt.show()
+
+def find_words(chars, boxes):
+    words = []
+    for i in xrange(boxes.shape[0]):
+        words.append(chars[i])
+        if int(boxes[i]["word_end"]) == 1:
+            words.append(" ")
+
+    words = "".join(words).split(" ")
+    return words
+
+
+def find_dimensions(boxes):
+
+    """Find the maximum height and width of any character from their bounding boxes."""
+
+    current_max_width = 0
+    current_max_height = 0
+    for i in xrange(boxes.shape[0]):
+        if (boxes[i]["right"] - boxes[i]["left"]) > current_max_width:
+            current_max_width = boxes[i]["right"] - boxes[i]["left"]
+
+        if (boxes[i]["top"] - boxes[i]["bottom"]) > current_max_height:
+            current_max_height = boxes[i]["top"] - boxes[i]["bottom"]
+
+    return current_max_width, current_max_height
+
+
+def generate_padded_images(input_data, boxes, result_array, max_width, max_height):
+
+    """Pad out and reshape (ravel) images to fit an array of equal sized 1-d feature vectors."""
+
+    for i in xrange(boxes.shape[0]):
+        left = boxes[i]["left"]
+        right = boxes[i]["right"]
+        bottom = input_data.shape[0] - boxes[i]["bottom"]
+        top = input_data.shape[0] - boxes[i]["top"]
+
+        image = np.reshape(input_data[top:bottom, left:right], (top-bottom, right-left))
+
+        padded_image = np.zeros((max_height, max_width), dtype=np.int)
+        padded_image.fill(255)
+
+        padded_image[0:bottom-top, 0:right-left] = image
+        result_array[i] = np.ravel(padded_image)
+
+    return result_array
+
+
+def prepare_images(page_data, page_boxes, max_width, max_height):
+    empty_images = np.zeros((page_boxes.shape[0], max_width * max_height), dtype=np.int)
+    page_images = generate_padded_images(page_data, page_boxes, empty_images, max_width, max_height)
+    return page_images
+
+
+def generate_eigenletters(train_data, feature_number):
+
+    """Generate eigenvalues from covariance matrix of training data."""
+    covx = np.cov(train_data, rowvar=0)
+    cov_size = covx.shape[0]
+    eigenvalues, eigenvectors = la.eigh(covx, eigvals=(cov_size - feature_number, cov_size - 1))
+    eigenvectors = np.fliplr(eigenvectors)
+    return eigenvectors
+
+
+def run_pca(num_features):
+    eigenletters = generate_eigenletters(images, num_features)
+    pcatrain_data = np.dot((images - np.mean(images)), eigenletters)
+    pcatest_data = np.dot((test_page1_prepared - np.mean(images)), eigenletters)
+
+    return pcatrain_data, pcatest_data
+
+
+def classify(train, train_data_labels, test, test_data_labels, features=None):
+
+    """Nearest neighbour classification implementation
+        by Jon Barker (http://staffwww.dcs.shef.ac.uk/people/J.Barker/)"""
+
+    # Use all features if no feature parameter has been supplied
+    if features is None:
+        features = np.arange(0, train.shape[1])
+
+    # Select the desired features from the training and test data
+    train = train[:, features]
+    test = test[:, features]
+
+    # Super compact implementation of nearest neighbour
+    x = np.dot(test, train.transpose())
+    modtest = np.sqrt(np.sum(test * test, axis=1))
+    modtrain = np.sqrt(np.sum(train * train, axis=1))
+
+    dist = x/np.outer(modtest, modtrain.transpose())  # cosine distance
+    nearest = np.argmax(dist, axis=1)
+
+    label = train_data_labels[nearest]
+    score = (100.0 * sum(test_data_labels[:] == label))/label.shape[0]
+    return score, label
+
+
+max_train_width, max_train_height = max(find_dimensions(page1_boxes),
+                                        find_dimensions(page2_boxes),
+                                        find_dimensions(page3_boxes),
+                                        find_dimensions(page4_boxes))
+
+max_test_width, max_test_height = find_dimensions(test1_boxes)
+
+max_width, max_height = max((max_train_width, max_train_height), (max_test_width, max_test_height))
+
+page1_prepared = prepare_images(page1, page1_boxes, max_width, max_height)
+page2_prepared = prepare_images(page2, page2_boxes, max_width, max_height)
+page3_prepared = prepare_images(page3, page3_boxes, max_width, max_height)
+page4_prepared = prepare_images(page4, page4_boxes, max_width, max_height)
+
+images = np.vstack((page1_prepared, page2_prepared, page3_prepared, page4_prepared))
+
+test_page1_prepared = prepare_images(test1, test1_boxes, max_width, max_height)
+# test_page2_prepared = prepare_images(test2, test2_boxes, max_width, max_height)
+
+train_labels = np.hstack((page1_boxes["labels"], page2_boxes["labels"], page3_boxes["labels"], page4_boxes["labels"]))
+test_labels = test1_boxes["labels"]
+
+
+num_features = 40
+pca_results = run_pca(num_features)
+pcatraindata = pca_results[0]
+pcatestdata = pca_results[1]
+classify_score, labelled_chars = classify(pcatraindata, train_labels, pcatestdata, test_labels, xrange(num_features))
+
+predicted_words = find_words(labelled_chars, test1_boxes)
+correct_words = find_words(test1_boxes["labels"], test1_boxes)
+
+print classify_score
+print predicted_words
+error_corrector = ErrorCorrector()
+corrected_words = error_corrector.correct_words(predicted_words, correct_words)
+print corrected_words
+
+
+def count_correct():
+    correct_count = 0
+    for i in xrange(len(corrected_words)):
+        if corrected_words[i] == correct_words[i]:
+            correct_count += 1
+
+    return 100.0 * correct_count / len(correct_words)
+
+print count_correct()
